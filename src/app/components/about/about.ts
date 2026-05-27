ce32c239b90fc10507ad5f2c553b85dcd43d1ce6
Danilo Mori <danilomori@Danilos-MacBook-Air.local>
2026-05-27 12:46:33 -0300

    New data

--- src/app/components/about/about.ts
+++ src/app/components/about/about.ts
@@ -11,7 +11,7 @@ import { CounterDirective } from '../../directives/counter.directive';
 export class AboutComponent {
   stats = [
     { numeric: 26, suffix: '+', label: 'Anos de experiência' },
-    { numeric: 5,  suffix: 'k+', label: 'Veículos atendidos' },
+    { numeric: 70,  suffix: 'k+', label: 'Veículos atendidos' },
     { numeric: 100, suffix: '%', label: 'Satisfação dos clientes' },
     { numeric: 24, suffix: 'h', label: 'Diagnóstico rápido' }
   ];
